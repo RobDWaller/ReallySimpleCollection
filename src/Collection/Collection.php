ef33d82da107042d195e48108f28f161273f4486
Rob Waller <rdwaller1984@gmail.com>
2017-05-07 15:27:31 +0100

    Added basic collection class with count and iterator contracts, added unit tests to test map and filter functionality.

---
+++ src/Collection/Collection.php
@@ -0,0 +1,59 @@
+<?php namespace ReallySimpleCollection\Collection;
+
+use Iterator;
+use Countable;
+
+class Collection implements Iterator, Countable
+{
+    private $collectionArray;
+
+    public function __construct(array $collectionArray)
+    {
+        $this->collectionArray = $collectionArray;
+    }
+
+    public function current()
+    {
+        return current($this->collectionArray);
+    }
+
+    public function next()
+    {
+        return next($this->collectionArray);
+    }
+
+    public function key()
+    {
+        return key($this->collectionArray);
+    }
+
+    public function rewind()
+    {
+        reset($this->collectionArray);
+    }
+
+    public function valid()
+    {
+        return isset($this->collectionArray[$this->key()]);
+    }
+
+    public function count()
+    {
+        return count($this->collectionArray);
+    }
+
+    public function filter($callback)
+    {
+        return new Collection(array_filter($this->collectionArray, $callback));
+    }
+
+    public function map($callback)
+    {
+        return new Collection(array_map($callback, $this->collectionArray));
+    }
+
+    public static function make(array $collectionArray)
+    {
+        return new static($collectionArray);
+    }
+}
---
+++ tests/CollectionTest.php
@@ -0,0 +1,117 @@
+<?php
+
+use ReallySimpleCollection\Collection\Collection;
+
+class CollectionCollectionTest extends PHPUnit_Framework_TestCase 
+{
+    public function testCollection()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+        
+        $collection = new Collection($array);
+
+        $this->assertInstanceOf(Collection::class, $collection);   
+    }
+
+    public function testCollectionLoop()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+        
+        $collection = new Collection($array);
+
+        foreach ($collection as $key => $value) {
+            $this->assertTrue(array_key_exists('surname', $value));
+        }
+    }
+
+    public function testCollectionFilter()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+
+        $collection = new Collection($array);
+
+        $result = $collection->filter(
+            function($var) {
+                return $var['age'] <= 30;
+            }
+        );
+
+        $this->assertInstanceOf(Collection::class, $result);
+
+        $this->assertEquals(3, $result->count());
+
+        $this->assertEquals('Craig', $result->next()['forename']);
+    }
+
+    public function testCollectionMap()
+    {   
+        $array = require(__DIR__ . '/UserArray.php');
+
+        $collection = new Collection($array);
+
+        $result = $collection->map(function($var) {
+            return $var['forename'] . ' ' . $var['surname']; 
+        });
+
+        $this->assertInstanceOf(Collection::class, $result);
+
+        $this->assertEquals(5, $result->count());
+
+        $this->assertEquals('John Galt', $result->current());
+
+        $this->assertEquals('Anne Jones', $result->next());
+    }
+
+    public function testCollectionAddItem()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+
+        $collection = new Collection($array);
+
+        $result = $collection->map(function($var) {
+            $var['fullname'] = $var['forename'] . ' ' . $var['surname'];
+            return $var;
+        });
+
+        $this->assertInstanceOf(Collection::class, $result);
+
+        $this->assertEquals(5, $result->count());
+
+        $this->assertEquals('John Galt', $result->current()['fullname']);
+
+        $this->assertEquals('John', $result->current()['forename']);
+    }
+
+    public function testCollectionRemoveKey()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+
+        $collection = new Collection($array);
+
+        $this->assertTrue(isset($collection->current()['email']));
+
+        $result = $collection->map(function($var) {
+            unset($var['email']);
+            return $var;
+        });
+
+        $this->assertInstanceOf(Collection::class, $result);
+
+        $this->assertEquals(5, $result->count());
+
+        $this->assertFalse(isset($result->current()['email']));
+
+        $this->assertFalse(isset($result->next()['email']));
+    }
+
+    public function testCollectionStaticMake()
+    {
+        $array = require(__DIR__ . '/UserArray.php');
+
+        $collection = Collection::make($array);
+
+        $this->assertInstanceOf(Collection::class, $collection);
+
+        $this->assertEquals('John', $collection->current()['forename']);
+    }
+}
