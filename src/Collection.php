8798efd8a7004135fa7a9e425212d302b5264da3
RobDWaller <rob@statuspeople.com>
2018-03-11 19:20:50 +0000

    Tidied up collection class, wrote some comments.

--- src/Collection.php
+++ src/Collection.php
@@ -4,61 +4,131 @@ namespace ReallySimple;
 
 use Iterator;
 use Countable;
+use Closure;
 
+/**
+ * A very simple collection library that helps you turn arrays into collections.
+ * provides lots of useful methods such as first, map, filter, etc.
+ *
+ * @author Rob Waller <rdwaller1984@googlemail.com>
+ */
 class Collection implements Iterator, Countable
 {
-    private $collectionArray;
 
-    public function __construct(array $collectionArray)
+    /**
+     * The collection array
+     *
+     * @var array $items
+     */
+    private $items;
+
+    /**
+     * @param array $items
+     */
+    public function __construct(array $items)
     {
-        $this->collectionArray = $collectionArray;
+        $this->items = $items;
     }
 
+    /**
+     * Retun the current item in the array
+     *
+     * @return mixed
+     */
     public function current()
     {
-        return current($this->collectionArray);
+        return current($this->items);
     }
 
+    /**
+     * Return the next item in the array
+     *
+     * @return mixed
+     */
     public function next()
     {
-        return next($this->collectionArray);
+        return next($this->items);
     }
 
+    /**
+     * Return the key that the array is currently on
+     *
+     * @return int | string
+     */
     public function key()
     {
-        return key($this->collectionArray);
+        return key($this->items);
     }
 
+    /**
+     * Reset the collection array to the start
+     *
+     * @return void
+     */
     public function rewind()
     {
-        reset($this->collectionArray);
+        reset($this->items);
     }
 
-    public function valid()
+    /**
+     * Check to see if the current array item is valid or isset
+     *
+     * @return bool
+     */
+    public function valid(): bool
     {
-        return isset($this->collectionArray[$this->key()]);
+        return isset($this->items[$this->key()]);
     }
 
-    public function count()
+    /**
+     * Return the count of the number of items in the collection array
+     *
+     * @return int
+     */
+    public function count(): int
     {
-        return count($this->collectionArray);
+        return count($this->items);
     }
 
-    public function filter($callback)
+    /**
+     * Filter the collection items via a callback / closure and return a new
+     * collection
+     *
+     * @param Closure $callback
+     * @return Collection
+     */
+    public function filter(Closure $callback): Collection
     {
-        return new Collection(array_filter($this->collectionArray, $callback));
+        return new Collection(array_filter($this->items, $callback));
     }
 
-    public function map($callback)
+    /**
+     * Map the collection items via a callback / closure and return a new
+     * collection
+     *
+     * @param Closure $callback
+     * @return Collection
+     */
+    public function map(Closure $callback): Collection
     {
-        return new Collection(array_map($callback, $this->collectionArray));
+        return new Collection(array_map($callback, $this->items));
     }
 
-    public function toArray()
+    /**
+     * Return the collection as an array
+     *
+     * @return array
+     */
+    public function toArray(): array
     {
-        return $this->collectionArray;
+        return $this->items;
     }
 
+    /**
+     * Return the first item from the collection
+     *
+     * @return mixed
+     */
     public function first()
     {
         $this->rewind();
@@ -66,8 +136,13 @@ class Collection implements Iterator, Countable
         return $this->current();
     }
 
-    public static function make(array $collectionArray)
+    /**
+     * Build the collection statically
+     *
+     * @return Collection
+     */
+    public static function make(array $items): Collection
     {
-        return new static($collectionArray);
+        return new static($items);
     }
 }
